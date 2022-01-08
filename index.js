693ff7113df5390fec148db7377dbd5616430edd
TahaM1 <tahanaeem12@hotmail.com>
2022-01-08 16:23:15 -0500

    refactered into function

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var ffmpeg = require('ffmpeg');
+var fs = require('fs')
 
 const {exec} = require('child_process');
+
 const { stdout, stderr } = require('process');
 
 const input_file = 'test.mp4'
 const output_file = 'test_frames/test_%03d.jpg'
-const cmd_line_args = `ffmpeg -i ${input_file} -qscale:v 2 ${output_file}`
 
-exec(cmd_line_args, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-})
+
+const extractImagesFromVideo = (input, output) => {
+    const cmd_line_args = `ffmpeg -i ${input} -qscale:v 2 ${output}`
+    exec(cmd_line_args, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+}
+
+extractImagesFromVideo(input_file, output_file);
 
